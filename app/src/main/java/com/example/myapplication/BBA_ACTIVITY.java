9b72ce59b4c61951e4e202930045a966d5a561fa
Swapnil <swapnilpatel212601@gmail.com>
2023-09-12 17:59:31 +0530

    AlL 4 pages completed

---
+++ app/src/main/java/com/example/myapplication/BBA_ACTIVITY.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BBA_ACTIVITY extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bba);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MCA_ACTIVITY.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MCA_ACTIVITY extends AppCompatActivity  {
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mca);
+
+    }
+
+
+
+}
